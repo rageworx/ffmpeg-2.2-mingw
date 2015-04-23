b9a123be8db6f71a210fd8cf13fec5794760019a
Raphael Kim <rageworx@gmail.com>
2015-04-23 16:15:28 +0900

    Updating configure for MinGW supporting YASM.
    You may check cfg4mingw.sh and YASM executive file name.

--- cfg4mingw.sh
+++ cfg4mingw.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+
+#test YASM
+YASM="vsyasm"
+YASM_TEST=`vsyasm --version`
+
+if [ -z "$YASM_TEST" ]; then
+echo "YASM not found."
 ./configure --disable-yasm --disable-iconv
+else
+echo "YASM found."
+./configure --disable-iconv --yasmexe=$YASM
+fi
